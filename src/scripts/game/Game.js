81b183e02a4d4ed017930682b4ad20aea04b40ac
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2023-05-09 21:25:18 +0200

    The Game class is responsible for creating and managing the game's scene, including creating and updating the background.

---
+++ src/scripts/game/Game.js
@@ -0,0 +1,17 @@
+import { App } from "../system/App";
+import { Scene } from "../system/Scene";
+import { Background } from "./Background";
+
+export class Game extends Scene {
+    create() {
+        this.createBackground();
+    }
+    createBackground() {
+        this.bg = new Background();
+        this.container.addChild(this.bg.container);
+    }
+    
+    update(dt) {
+        this.bg.update(dt);
+    }
+}
