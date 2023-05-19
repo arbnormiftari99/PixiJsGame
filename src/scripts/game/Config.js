027a39cad98d8b4be8dad3d7436f7bceb7ccb8dc
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2023-05-19 21:58:33 +0200

    Created config for hero position and speed of the hero

--- src/scripts/game/Config.js
+++ src/scripts/game/Config.js
@@ -3,6 +3,28 @@ import { Tools } from "../system/Tools";
 
 export const Config = {
     bgSpeed: 2,
+    platforms: {
+        ranges: {
+            rows: {
+                min: 2,
+                max: 6
+            },
+            cols: {
+                min: 3,
+                max: 9
+            },
+            offset: {
+                min: 60,
+                max: 200
+            }
+        }
+    },
+    hero: {
+        position: {
+            x: 350,
+            y: 340
+        }
+    },
     loader: Tools.massiveRequire(require["context"]('./../../sprites/', true, /\.(mp3|png|jpe?g)$/)),
     scenes: {
         "Game": Game
