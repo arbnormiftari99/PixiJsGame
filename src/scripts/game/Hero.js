1c49f4d12702be64226d4324cf4d5b6c8de055ba
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2023-05-22 23:28:39 +0200

    Added matter js as physics Engine, Check if jumpindex <=2, Rectangle function

--- src/scripts/game/Hero.js
+++ src/scripts/game/Hero.js
@@ -1,9 +1,46 @@
 import * as PIXI from "pixi.js";
 import { App } from '../system/App';
+import * as Matter from 'matter-js';
+
 
 export class Hero {
     constructor() {
         this.createSprite();
+        this.createBody();
+        App.app.ticker.add(this.update.bind(this));
+
+        this.dy = App.config.hero.jumpSpeed;
+        this.maxJumps = App.config.hero.maxJumps;
+        this.jumpIndex = 0;
+
+    }
+
+    startJump() {
+        if (this.platform || this.jumpIndex === 1) {
+            ++this.jumpIndex;
+            this.platform = null;
+            Matter.Body.setVelocity(this.body, { x: 0, y: -this.dy });
+        }
+     
+      
+    }
+
+
+    stayOnPlatform(platform) {
+        this.platform = platform;
+        this.jumpIndex = 0;
+    }
+
+    createBody() {
+        this.body = Matter.Bodies.rectangle(this.sprite.x + this.sprite.width / 2, this.sprite.y + this.sprite.height / 2, this.sprite.width, this.sprite.height, {friction: 0});
+        Matter.World.add(App.physics.world, this.body);
+        this.body.gameHero = this;
+    }
+
+
+    update() {
+        this.sprite.x = this.body.position.x - this.sprite.width / 2;
+        this.sprite.y = this.body.position.y - this.sprite.height / 2;
     }
 
     createSprite() {
