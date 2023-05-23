1f08674edf65a6d338bdd966c9e9580edccff5de
arbnormiftari99 <117682007+arbnormiftari99@users.noreply.github.com>
2023-05-23 20:40:36 +0200

    Added different sprite pic, showing randomly assigning a higher probability value to the "money-bag" Pic in the random selection process

--- src/scripts/game/Diamond.js
+++ src/scripts/game/Diamond.js
@@ -1,16 +1,41 @@
+import * as Matter from 'matter-js';
 import { App } from '../system/App';
 
+
+
 export class Diamond {
+      
     constructor(x, y) {
-        this.sprite = App.sprite("diamond");
         this.createSprite(x, y);
+        App.app.ticker.add(this.update.bind(this));
 
     }
 
+
     createSprite(x, y) {
-        this.sprite = App.sprite("diamond");
+        const images = ['money', 'diamond', 'money-bag']; // Array of available images
+        const probabilities = [0.5, 0.4, 0.1]; // Probabilities corresponding to each image (should add up to 1)
+        
+        let randomImage;
+        const randomValue = Math.random();
+        let cumulativeProbability = 0;
+        for (let i = 0; i < images.length; i++) {
+          cumulativeProbability += probabilities[i];
+          if (randomValue <= cumulativeProbability) {
+            randomImage = images[i];
+            break;
+          }
+        }
+    
+        this.sprite = App.sprite(randomImage);
         this.sprite.x = x;
         this.sprite.y = y;
+      }
+
+    update() {
+        if (this.sprite) {
+            Matter.Body.setPosition(this.body, {x: this.sprite.width / 2 + this.sprite.x + this.sprite.parent.x, y: this.sprite.height / 2 + this.sprite.y + this.sprite.parent.y});
+        }
     }
 
     createBody() {
@@ -19,4 +44,6 @@ export class Diamond {
         this.body.isSensor = true;
         Matter.World.add(App.physics.world, this.body);
     }
+
+
 }
